1180a03b795e9a8dc80b86a314a25bd9e45efa74
mauro-titanio <maurotitanio@gmail.com>
2021-07-31 09:46:38 +0200

    init

---
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,29 @@
+// src\app\shared\components\sidenav\sidenav.component.ts
+
+import { Component, OnInit, Input } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+})
+export class SidenavComponent implements OnInit {
+  themeColor: 'primary' | 'accent' | 'warn' = 'primary'; // ? notice this
+  isDark = false; // ? notice this
+  constructor(private overlayContainer: OverlayContainer) {}
+
+  ngOnInit(): void {}
+
+  // ? notice below
+  toggleTheme(): void {
+    this.isDark = !this.isDark;
+    if (this.isDark) {
+      this.overlayContainer.getContainerElement().classList.add('dark-theme');
+    } else {
+      this.overlayContainer
+        .getContainerElement()
+        .classList.remove('dark-theme');
+    }
+  }
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+@NgModule({
+  declarations: [
+    SidenavComponent
+  ],
+  imports: [CommonModule, MaterialModule], // ? changed
+  exports: [SidenavComponent], 
+})
+export class SharedModule {}
